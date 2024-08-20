139009d83ebbe71f3f4614d1361a08642c068145
Jonathan-Ultini <jonathanultini@gmail.com>
2024-08-20 15:37:20 +0200

    milestone 2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log('OK')
+// Array di immagini
+const images = [
+  "img/01.webp",
+  "img/02.webp",
+  "img/03.webp",
+  "img/04.webp",
+  "img/05.webp"
+];
+
+// Selezioniamo il contenitore del carosello
+const carouselContainer = document.querySelector('.carousel');
+
+// Ciclo per inserire le immagini dinamicamente
+for (let i = 0; i < images.length; i++) {
+  const imgElement = document.createElement('img');
+  imgElement.src = images[i];
+  imgElement.alt = `Immagine ${i + 1}`;
+  imgElement.classList.add('carousel-img');
+  if (i === 0) imgElement.classList.add('active');
+  carouselContainer.appendChild(imgElement);
+}
